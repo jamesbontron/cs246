047bd8d70a1b2d30c77e7d98ef135bb46e03a7c4
tonisud777 <antonrodaragon@gmail.com>
2021-01-29 11:04:36 -0600

    Massage learning to use

--- HelloFromAlejandro/src/naranjo/Main.java
+++ HelloFromAlejandro/src/naranjo/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Alejandro");
 	    System.out.println("Hello Alejo my name is Luis Rodriguez");
-	    
+	    System.out.println("Learning how to use IntelliJ");
     }
 }
