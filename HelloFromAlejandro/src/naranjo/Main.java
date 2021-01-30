bc1177741acbd58e0cafe7b3a76b6f8aae6eab22
Rayshorn Richardson <rayshorndc@gmail.com>
2021-01-29 20:12:22 -0400

    Update from Rayshorn Richardson

--- HelloFromAlejandro/src/naranjo/Main.java
+++ HelloFromAlejandro/src/naranjo/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Alejandro");
 	    System.out.println("Hello Alejo my name is Luis Rodriguez");
+	    System.out.println("Hello guys this is Rayshorn :)");
 	    System.out.println("Learning how to use IntelliJ");
 		System.out.println("fixing pepe");
 		System.out.println("fixing pepe0.1");
